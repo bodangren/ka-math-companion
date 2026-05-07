0778305276aa402ce669a26926940adddded771d
Daniel Bo <bodangren@gmail.com>
2026-05-07 10:40:47 +0800

    feat(progress): add LocalStorage ports for progress persistence (Task 2)

--- src/Ports.elm
+++ src/Ports.elm
@@ -1,7 +1,25 @@
-port module Ports exposing (processMath, renderMath)
+port module Ports exposing (processMath, renderMath, saveProgress, loadProgress, exportProgress, importProgress, progressReceived, progressImported)
 
 
 port renderMath : { id : String, latex : String, displayMode : Bool } -> Cmd msg
 
 
 port processMath : String -> Cmd msg
+
+
+port saveProgress : String -> Cmd msg
+
+
+port loadProgress : String -> Cmd msg
+
+
+port exportProgress : String -> Cmd msg
+
+
+port importProgress : String -> Cmd msg
+
+
+port progressReceived : (String -> msg) -> Sub msg
+
+
+port progressImported : (String -> msg) -> Sub msg
